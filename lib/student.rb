c632c7a124f6f2bba1a981726b4085634c7be121
Greg Stimson <stimsong@yahoo.com>
2018-04-26 23:47:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,19 +4,29 @@ class Student
 
   @@all = []
 
-  def initialize#(student_hash)  commented out by GRS
-
+  def initialize(student_hash)
+    student_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    # @name = student_hash[:name]
+    # @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |att, val|
+      self.send("#{att}=", val)
+    end
+    self
   end
 
   def self.all
-
+    @@all
   end
 end
