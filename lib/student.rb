46019dc775744539a79c31849893dcdd2526dd81
Greg Stimson <stimsong@yahoo.com>
2018-04-26 23:33:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,6 @@ class Student
     # @name = student_hash[:name]
     # @location = student_hash[:location]
     @@all << self
-    binding.pry
   end
 
   def self.create_from_collection(students_array)
